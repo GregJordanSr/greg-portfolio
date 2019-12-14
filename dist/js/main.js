7ec9c9a3b32b9763e1448c9d835f74312309132c
GregJordanSr <gregljordan@gmail.com>
2019-12-14 17:27:42 -0500

    Completed toggle menu, and menu btn animations

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,34 @@
 // Adding functionality to the menu
 
-const menuBtn = document.querySelector('.btn-menu');
+const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
-const menuBtn = document.querySelector('.btn-menu');
-const menuBtn = document.querySelector('.btn-menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+const navItems = document.querySelectorAll('.nav-item');
+
+// Set default state of menu
+let showMenu = false;
+
+menuBtn.addEventListener('click', changeState);
+
+function changeState() {
+    if (!showMenu) {
+        menuBtn.classList.add('close');
+        menu.classList.add('show');
+        menuNav.classList.add('show');
+        menuBranding.classList.add('show');
+        navItems.forEach(item => item.classList.add('show'));
+
+        // change menu state
+        showMenu = true;
+    } else {
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        menuBranding.classList.remove('show');
+        navItems.forEach(item => item.classList.remove('show'));
+
+        // change menu state
+        showMenu = false;
+    }
+}
