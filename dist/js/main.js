ecf3949c2e9e8a118c52a271b9dde12fc6e1cc31
GregJordanSr <gregljordan@gmail.com>
2019-12-14 15:22:05 -0500

    Adding javascript

---
+++ dist/js/main.js
@@ -0,0 +1,6 @@
+// Adding functionality to the menu
+
+const menuBtn = document.querySelector('.btn-menu');
+const menu = document.querySelector('.menu');
+const menuBtn = document.querySelector('.btn-menu');
+const menuBtn = document.querySelector('.btn-menu');
